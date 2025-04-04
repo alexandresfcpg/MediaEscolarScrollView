704664097d9f7ec9085ec7bb62a5732ec8e66900
alexandresfcpg <alexandresfcpg@yahoo.com.br>
2025-04-03 21:12:42 -0300

    Ciclo de vida do aplicativo

--- app/src/main/java/app/daazi/aluno/mediaescolarscrollview/MainActivity.java
+++ app/src/main/java/app/daazi/aluno/mediaescolarscrollview/MainActivity.java
@@ -123,4 +123,30 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+    }
 }
